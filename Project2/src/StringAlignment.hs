e524b29193599637b0374ab7ed1561f7ecfed3ab
salvaCasta276 <salCas276@gmail.com>
2023-05-07 20:59:23 -0300

    Working version of the project that satisfies requirements

--- Project2/src/StringAlignment.hs
+++ Project2/src/StringAlignment.hs
@@ -1,13 +1,15 @@
 -- Salvador Castagnino, Aden McCusker
 import Data.List
+import Text.Printf
 
 main :: IO ()
 main = do
-  let score = newSimilarityScore "writers" "vintner"
-  putStrLn ("The score is: " ++ show score)
+  --let score = newSimilarityScore "aferociousmonadatemyhamster" "functionalprogrammingrules"
+  --putStrLn ("The score is: " ++ show score)
   --let maxLens = maximaBy length ["cs", "efd", "lth", "it"]
   --putStrLn ("The maxLens are: " ++ show maxLens)
-  outputOptAlignments "writers" "vintner"
+  --outputOptAlignments "aferociousmonadatemyhamster" "functionalprogrammingrules"
+  outputOptAlignments "bananrepubliksinvasionsarmestabsadjutant" "kontrabasfiolfodralmakarmästarlärling"
 
 scoreSpace = -1
 score(x, '-') = scoreSpace
@@ -24,15 +26,15 @@ similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score(x, y),
 newSimilarityScore :: String -> String -> Int
 newSimilarityScore xs ys = simCache (length xs) (length ys)
   where
-    simCache i j = mcsTable!!i!!j
-    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
+    simCache i j = simTable!!i!!j
+    simTable = [[ simEntry i j | j<-[0..]] | i<-[0..] ]
        
-    mcsEntry :: Int -> Int -> Int
-    mcsEntry i 0 = scoreSpace * i
-    mcsEntry 0 j = scoreSpace * j
-    mcsEntry i j = maximum [simCache (i-1) (j-1) + score(x, y),
-                            scoreSpace + simCache (i-1) j,
-                            scoreSpace + simCache i (j-1)]
+    simEntry :: Int -> Int -> Int
+    simEntry i 0 = scoreSpace * i
+    simEntry 0 j = scoreSpace * j
+    simEntry i j = maximum [simCache (i-1) (j-1) + score(x, y),
+                            score(x, '-') + simCache (i-1) j,
+                            score(y, '-') + simCache i (j-1)]
       where
          x = xs!!(i-1)
          y = ys!!(j-1)
@@ -48,31 +50,37 @@ type AlignmentType = (String,String)
 optAlignments :: String -> String -> [AlignmentType]
 optAlignments xs [] = [(xs, take (length xs) (repeat '-'))]
 optAlignments [] ys = [(take (length ys) (repeat '-'), ys)]
-optAlignments (x:xs) (y:ys) = concat (map (\(a, b) -> (uncurry attachHeads a) (uncurry optAlignments b)) maxims)
+optAlignments (x:xs) (y:ys) = concat (map (\(a, b) -> (uncurry attachHeads a) (uncurry optAlignments b)) opts)
   where
-    maxims = maximaBy (\(a, b) -> score a + uncurry similarityScore b) [((x, y), (xs, ys)),
+    opts = maximaBy (\(a, b) -> score a + uncurry similarityScore b) [((x, y), (xs, ys)),
                                                                         ((x, '-'), (xs, (y:ys))),
                                                                         (('-', y), ((x:xs), ys))]
-      where
 
-newSimilarityScore :: String -> String -> Int
-newSimilarityScore xs ys = simCache (length xs) (length ys)
+newOptAlignments :: String -> String -> [AlignmentType]
+newOptAlignments xs ys = snd (optCache (length xs) (length ys))
   where
-    simCache i j = mcsTable!!i!!j
-    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
+    optCache i j = optTable!!i!!j
+    optTable = [[ optEntry i j | j<-[0..]] | i<-[0..] ]
        
-    mcsEntry :: Int -> Int -> Int
-    mcsEntry i 0 = scoreSpace * i
-    mcsEntry 0 j = scoreSpace * j
-    mcsEntry i j = maximum [simCache (i-1) (j-1) + score(x, y),
-                            scoreSpace + simCache (i-1) j,
-                            scoreSpace + simCache i (j-1)]
+    optEntry :: Int -> Int -> (Int, [AlignmentType])
+    optEntry i 0 = (scoreSpace * i, [(reverse (take i (reverse xs)), take i (repeat '-'))])
+    optEntry 0 j = (scoreSpace * j, [(take j (repeat '-'), reverse (take j (reverse ys)))])
+    optEntry i j = ((maximum . fst . unzip) opts, (concat . snd . unzip . (maximaBy fst)) opts)
       where
-         x = xs!!(i-1)
-         y = ys!!(j-1)
+         x = xs!!(length xs - i)
+         y = ys!!(length ys - j)
+         opts = [(fst (optCache (i-1) (j-1))  + score(x, y), attachHeads x y (snd (optCache (i-1) (j-1)))),
+                 (fst (optCache (i-1) j) + score(x, '-'), attachHeads x '-' (snd (optCache (i-1) j))),
+                 (fst (optCache i (j-1)) + score('-', y), attachHeads '-' y (snd (optCache i (j-1))))]
 
 outputOptAlignments :: String -> String -> IO ()
-outputOptAlignments string1 string2 = print (optAlignments string1 string2)
+outputOptAlignments string1 string2 = do
+  printf "There are %d optimal alignments:\n\n" (length alignments)
+  mapM_ printPair alignments
+  printf "There were %d optimal alignments!\n" (length alignments)
+    where
+      printPair (x, y) = putStrLn (x ++ "\n" ++ y ++ "\n")
+      alignments = newOptAlignments string1 string2
 
 --There are 3 optimal alignments:
 
